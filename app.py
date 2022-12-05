b7881eba0229aab33149ffa4418aa1f2afe74355
azizp128 <azizprabowo128@gmail.com>
2022-12-05 20:40:00 +0700

    new code

--- app.py
+++ app.py
@@ -9,26 +9,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 cols=['age','workclass','education','marital-status','occupation','relationship','race','gender','capital-gain','capital-loss',
       'hours-per-week','native-country']
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# prediction function
+def ValuePredictor(to_predict_list):
+	to_predict = np.array(to_predict_list).reshape(1, 12)
+	loaded_model = pickle.load(open("model.pkl", "rb"))
+	result = loaded_model.predict(to_predict)
+	return result[0]
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    feature_list = request.form.to_dict()
-    feature_list = list(feature_list.values())
-    feature_list = list(map(int, feature_list))
-    final_features = np.array(feature_list).reshape(1, 12) 
-    
-    prediction = model.predict(final_features)
-    output = int(prediction[0])
-    if output == 1:
-        text = ">50K"
-    else:
-        text = "<=50K"
-
-    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
-
+@app.route('/result', methods = ['POST'])
+def result():
+	if request.method == 'POST':
+		to_predict_list = request.form.to_dict()
+		to_predict_list = list(to_predict_list.values())
+		to_predict_list = list(map(int, to_predict_list))
+		result = ValuePredictor(to_predict_list)	
+		if int(result)== 1:
+			prediction ='Income more than 50K'
+		else:
+			prediction ='Income less that 50K'		
+		return render_template("result.html", prediction = prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
